5cb006502547f1df63fff63dd6664fdcc30732c4
Kevin Atherton <katherto@gmail.com>
2021-01-18 20:37:38 -0600

    day 1 progress

--- pages/d1.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Day 1 Exercise</h1>
-  </div>
-</template>
-
-<script>
-export default {}
-</script>
---
+++ pages/day1/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Day 1 Exercise</h1>
+    <textarea
+      id="puzzleData"
+      v-model="puzzleData"
+      name="puzzle_data"
+      cols="30"
+      rows="10"
+    ></textarea>
+    <input type="button" value="Find Solution" @click="solve()" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      puzzleData: '',
+    };
+  },
+  computed: {
+    puzzleArray() {
+      return this.puzzleData.split('\n').map((item) => parseInt(item));
+    },
+  },
+  methods: {
+    solve() {},
+  },
+};
+</script>
